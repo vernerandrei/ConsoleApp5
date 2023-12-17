46822e7582e830dbc48969e41d66204d843a075d
Пользователь <@DESKTOP-E7N1RLV>
2023-12-18 00:55:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Задача 1.Используя двузначное случайное число, вывести на экран информацию о четности или нечетности этого числа с использованием тернарной операции.
+            //    Random rnd = new Random();
+            //    int number = rnd.Next(10, 99);
+            //    Console.WriteLine($"number={number}");
+            //    string stroka = (number % 2 == 0) ? "четное число" : "нечетное число";
+            //    Console.WriteLine($"{stroka }");
+            //    Console.ReadKey();
+            //}
+
+
+            //Задача 2.Используя случайное число в диапазоне от 1 до 7, вывести на экран название дня недели.
+            //Random rnd = new Random();
+            //int number = rnd.Next(1, 7);
+            //Console.WriteLine($"number= {number}");
+            //switch (number)
+            //{
+
+            //    case 1: Console.WriteLine("понедельник"); break;
+            //    case 2: Console.WriteLine("вторник"); break;
+            //    case 3: Console.WriteLine("среда"); break;
+            //    case 4: Console.WriteLine("четверг"); break;
+            //    case 5: Console.WriteLine("пятница"); break;
+            //    case 6: Console.WriteLine("суббота"); break;
+            //    case 7: Console.WriteLine("воскресенье"); break;
+
+            //}
+            //Console.ReadKey();
+
+
+            //Задача 4.Проведен тест, оцениваемый в целочисленных баллах от нуля до ста. Вывести на экран оценку тестируемого в зависимости от набранного количества баллов: от 90 до 100 – «отлично», от 70 до 89 – «хорошо», от 50 до 69 – «удовлетворительно», менее 50 – «неудовлетворительно».
+            //Console.Write("N= ");
+            //byte n = byte.Parse(Console.ReadLine());
+            //switch (n)
+            //{
+            //    case 1: Console.WriteLine("90,100 Отлично"); break;
+            //    case 2: Console.WriteLine("70, 89 Хорошо"); break;
+            //    case 3: Console.WriteLine("50, 69 Удовлетворительно"); break;
+            //    case 4: Console.WriteLine("0 , 49 Неудовлетвориельно"); break;
+
+            //}
+            //Console.ReadKey();
+
+
+               
+            }
+        }
+    }
+ 
+
